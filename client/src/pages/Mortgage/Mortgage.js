fd2a6c18ab568379d10a1827f5501179b5aedefc
dogdays1 <scarrc@bellsouth.net>
2018-08-08 23:03:48 -0400

    mortgageCalculator

---
+++ client/src/pages/Mortgage/Mortgage.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import MortgageCalculator from "mortgage-calculator-react";
+// import customStyle from "./Mortgage.css";
+import { Col, Row, Wrapper } from "../../components/Grid";
+
+class Mortgage extends Component {
+  state = {
+
+  };
+  
+  
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+  };
+
+  render() {
+    return (
+        <Wrapper>
+            <Row>
+                <Col size="3">
+
+                </Col>
+                <Col size="6">
+                <div className="mc">
+                    {/* <MortgageCalculator styles={customStyle} /> */}
+                    <MortgageCalculator />
+
+                </div>
+                </Col>
+                <Col size="3">
+ 
+                </Col>
+            </Row>
+        </Wrapper>
+    );
+  }
+}
+
+export default Mortgage;
