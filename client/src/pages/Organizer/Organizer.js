fdde4c448f5de50c072581bbd05bcf41a97f6e07
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-08-11 23:15:13 -0400

    added some features, drop down, datepicker, delete buttons to spend/bill

--- client/src/pages/Organizer/Organizer.js
+++ client/src/pages/Organizer/Organizer.js
@@ -1,87 +1,86 @@
 import React, { Component } from "react";
 import { Card, PieChartCard } from "../../components/Card";
 import { Col, Row, Wrapper } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import DeleteBtn from "../../components/DeleteBtn";
+import moment from "moment";
 import API from "../../utils/API";
 import "./Organizer.css";
-
- 
 
 class Organizer extends Component {
   state = {
     data: [
-        {title: "Data 1", value: 100, color: "red"},
-        {title: "Data 2", value: 60, color: "blue"},
-        {title: "Data 3", value: 30, color: "green"},
-        {title: "Data 4", value: 20, color: "orange"},
-        {title: "Data 5", value: 10, color: "grey"},
-    ]
-    // upcomingBills: [
-    //     {
-    //         title: "",
-    //         category: "",
-    //         amount: "",
-    //         date: ""
-    //     }
-    // ]
+      { title: "Data 1", value: 100, color: "red" },
+      { title: "Data 2", value: 60, color: "blue" },
+      { title: "Data 3", value: 30, color: "green" },
+      { title: "Data 4", value: 20, color: "orange" },
+      { title: "Data 5", value: 10, color: "grey" }
+    ],
+    upcomingBills: []
   };
-    // When the component mounts, load all the bills into the calender
-    componentDidMount() {
-        this.getUpcomingBills();
-    }
-   
-    // Loads all the upcomingBills into the calender
-    getUpcomingBills = () => {
-        API.getBills()
-        .then(res => {
-            console.log(res);
-            // const upcomingBills = res.data.map(event => ({
-            // title: event.payee,
-            // category: event.category,
-            // amount: event.amount,
-            // date: event.dueDate
-            // }));
-            this.setState({  });
-        })
-        .catch(err => console.error(err));
-    }
+  // When the component mounts, load all the bills into the calender
+  componentDidMount() {
+    this.getUpcomingBills();
+  }
+
+  // Loads all the upcomingBills into the calender
+  getUpcomingBills = () => {
+    API.getBills()
+      .then(res => {
+        console.log(res);
+        this.setState({ upcomingBills: res.data });
+      })
+      .catch(err => console.error(err));
+  };
+
+  // Delete a upcoming bill from database
+  deleteBillItem = id => {
+    API.deleteBill(id)
+      .then(res => this.getUpcomingBills())
+      .catch(err => console.log(err));
+  };
 
   handleFormSubmit = event => {
     event.preventDefault();
-
   };
 
   render() {
     return (
-        <Wrapper>
-            <Row>
-                <Col size="4">
-                    <Card
-                        title="Income"
-                        body1="$2480.00"     
-                    />
-                    <Card
-                        title="Amount Saved (current month)"
-                        body1={this.state.savings}
-                        body2="$580.00"
-                    />
-                </Col>
-                <Col size="4">
-                    <PieChartCard
-                        title="Spending"
-                        cardCategory=""
-                        data={this.state.data}
-                        
-                        />
-                </Col>
-                <Col size="4">
-                    <Card
-                        title="Upcoming Bills"
-                        body1={this.state.upcomingBills}
-                    />
-                </Col>
-   
-            </Row>
-        </Wrapper>
+      <Wrapper>
+        <Row>
+          <Col size="4">
+            <Card title="Income" body1="$2480.00" />
+            <Card
+              title="Amount Saved (current month)"
+              body1={this.state.savings}
+              body2="$580.00"
+            />
+          </Col>
+          <Col size="4">
+            <PieChartCard
+              title="Spending"
+              cardCategory=""
+              data={this.state.data}
+            />
+          </Col>
+          <Col size="4">
+            <Card title="Upcoming Bills" />
+            <List>
+              {this.state.upcomingBills.map(bill => (
+                <ListItem
+                  key={bill._id}
+                  heading={bill.payee}
+                  category={bill.category}
+                  amount={bill.amount}
+                  date={moment(bill.dueDate).format("MM-DD-YY")}
+                >
+                  <DeleteBtn onClick={() => this.deleteBillItem(bill._id)} />
+                </ListItem>
+              ))}
+            </List>
+          </Col>
+        </Row>
+      </Wrapper>
     );
   }
 }
--- client/src/pages/Spending/Spending.js
+++ client/src/pages/Spending/Spending.js
@@ -1,22 +1,13 @@
 import React, { Component } from "react";
 import { Col, Row, Wrapper } from "../../components/Grid";
 import { Input, FormBtn } from "../../components/Form";
-import { TableHead, TableRow } from "../../components/Table";
 import API from "../../utils/API";
 import "./Spending.css";
 import { List, ListItem } from "../../components/List";
 import DeleteBtn from "../../components/DeleteBtn";
-import moment from "moment"
-// import moment from "moment";
-
-// const dateFormat = require('dateformat');
-
-// const dateFormatter = date => {
-//     dateFormat(date, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-// }
-
-// const fomatted_date = moment(res.data.date).format('DD-MM-YYYY');        
-
+import moment from "moment";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 class Spending extends Component {
@@ -28,23 +19,27 @@ class Spending extends Component {
     spendings: []
   };
 
-  
-
   // When the component mounts, load all the spendings into the table
   componentDidMount() {
-      this.loadSpendings();
+    this.loadSpendings();
   }
 
   // Loads all the spending from the database
   loadSpendings = () => {
     API.getAllSpending()
       .then(res => {
-        this.setState({ spendings: res.data, item: "", category: "", amount: "", date: "" })
+        this.setState({
+          spendings: res.data,
+          item: "",
+          category: "",
+          amount: "",
+          date: ""
+        });
       })
       .catch(err => console.log(err));
   };
 
-
+  // Handle input change method
   handleInputChange = event => {
     const { name, value } = event.target;
 
@@ -54,94 +49,120 @@ class Spending extends Component {
   };
 
 
-  handleFormSubmit = event => {
+  // Handle change for the date in the date picker
+  handleChange = date => {
+    this.setState({
+      date: date,
       
-    event.preventDefault();
-    if (this.state.item && this.state.category && this.state.amount) {
-        API.saveSpending({
-            item: this.state.item,
-            category: this.state.category,
-            amount: this.state.amount,
-            date: this.state.date
-        })      // need to load spending
-            .then(res => this.loadSpendings())
-            .catch(err => console.log("Front end error" + err));
-
-    } else {
-        return alert("Please fill out all inputs");
-    }
-
+    })
   };
-  
+
+  // Delete a spending item from the database
+  deleteSpendItem = id => {
+    API.deleteSpending(id)
+      .then(res => this.loadSpendings())
+      .catch(err => console.log(err));
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.item && this.state.category && this.state.amount && this.state.date) {
+      API.saveSpending({
+        item: this.state.item,
+        category: this.state.category,
+        amount: this.state.amount,
+        date: this.state.date
+      }) // need to load spending
+        .then(res => this.loadSpendings())
+        .catch(err => console.log("Front end error" + err));
+    } else {
+      return alert("Please fill out all inputs");
+    }
+  };
 
   render() {
     return (
-        <Wrapper>
-            <Row>
-                <Col size="4">
-                    <form>
-                        <label className="spending-label">
-                            Recent Spending:
-                        </label>
-                        <Input
-                            value={this.state.item}
-                            onChange={this.handleInputChange}
-                            name="item"
-                            placeholder="Item name"
-                        />
-                        <Input
-                            value={this.state.category}
-                            onChange={this.handleInputChange}
-                            name="category"
-                            placeholder="Category"
-                        />
-                        <Input
-                            value={this.state.amount}
-                            onChange={this.handleInputChange}
-                            name="amount"
-                            placeholder="Enter amount"
-                        />
-                        <Input
-                            value={this.state.date}
-                            onChange={this.handleInputChange}
-                            name="date"
-                            placeholder="Enter date ex. 01/12/2018"
-                        />
-                        <FormBtn
-                            disabled={!(this.state.item && this.state.category && this.state.amount)}
-                            onClick={this.handleFormSubmit}
-                        >
-                            Submit 
-                        </FormBtn>
-                    </form>
-                </Col>
-                <Col size="1">
-                </Col>
-                <Col size="7">
-                <div className="table">
-                    <h1 className="spendingList">Spending List</h1>
-                        {this.state.spendings.length ? (
-                            <List>
-                                {this.state.spendings.map(spend => (
-                                <ListItem key={spend._id}
-                                    heading={spend.item}
-                                    category={spend.category}
-                                    amount={spend.amount}
-                                    date={moment(spend.date).format("MM-DD-YY hh:mm")}
-                                    >
-                                    <DeleteBtn onClick={() => this.deleteSpendItem(spend._id)}/>
-                                </ListItem>
-            
-                        ))}
-                        </List>
-                        ) : (
-                            <h3>Add something to spending list</h3>
-                        )}
- 
+      <Wrapper>
+        <Row>
+          <Col size="4">
+            <form>
+              <label className="spending-label">Recent Spending:</label>
+              <Input
+                value={this.state.item}
+                onChange={this.handleInputChange}
+                name="item"
+                placeholder="Item name"
+              />
+              <div className="form-group">
+                <select className="custom-select" id="inputGroupSelect01"
+                    value={this.state.category}
+                    onChange={this.handleInputChange} 
+                    >
+                    <option name="category" value="" disabled selected>Category</option>
+                    <option name="category" value="Food">Food</option>
+                    <option name="category" value="Entertainment">Entertainment</option>
+                    <option name="category" value="Car">Car</option>
+                    <option name="category" value="Family">Family</option>
+                    <option name="category" value="Shopping">Shopping</option>
+                    <option name="category" value="Gifts">Gifts</option>
+                    <option name="category" value="Investment">Investment</option>
+                    <option name="category" value="Vacation">Vacation</option>
+                    <option name="category" value="Emergency">Emergency</option>
+                    <option name="category" value="Misc">Misc</option>
+                </select>
+              </div>
+              
+              <Input
+                value={this.state.amount}
+                onChange={this.handleInputChange}
+                name="amount"
+                placeholder="Enter amount"
+              />
+                <div className="form-group">
+                <DatePicker
+                  selected={this.state.date}
+                  onChange={this.handleChange.bind(this)}
+                  placeholderText="Date"
+                />                
                 </div>
-                </Col>
-            </Row>
-        </Wrapper>
+
+              <FormBtn
+                disabled={
+                  !(this.state.item && this.state.category && this.state.amount && this.state.date)
+                }
+                onClick={this.handleFormSubmit}
+              >
+                Submit
+              </FormBtn>
+            </form>
+          </Col>
+          <Col size="1" />
+          <Col size="7">
+            <div className="table">
+              <h1 className="spendingList">Spending List</h1>
+              {this.state.spendings.length ? (
+                <List>
+                  {this.state.spendings.map(spend => (
+                    <ListItem
+                      key={spend._id}
+                      heading={spend.item}
+                      category={spend.category}
+                      amount={spend.amount}
+                      date={moment(spend.date).format("MM-DD-YY")}
+                    >
+                      <DeleteBtn
+                        onClick={() => this.deleteSpendItem(spend._id)}
+                      />
+                    </ListItem>
+                  ))}
+                </List>
+              ) : (
+                <h3>  </h3>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </Wrapper>
     );
   }
 }
