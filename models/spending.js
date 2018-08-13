a6195bc8b3c3c47ee924f271436517418842d6da
Luz_Hernandez <hernandezluz18@gmail.com>
2018-08-13 14:11:28 -0400

    spending user Auth

--- models/spending.js
+++ models/spending.js
@@ -5,7 +5,9 @@ const spendingSchema = new Schema({
   item: { type: String, required: true },
   category: { type: String, required: true },
   amount: { type: Number, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  //added email to associate user to their spending
+  email: {type: String,required: true}
 });
 
 const Spending = mongoose.model("Spending", spendingSchema);
