2ae964ffdaa5c022e48c0c5613f2fc181c895c09
dogdays1 <scarrc@bellsouth.net>
2018-08-14 10:58:09 -0400

    liveIntRatesAdded

--- client/src/pages/Mortgage/Mortgage.js
+++ client/src/pages/Mortgage/Mortgage.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import MortgageCalculator from "mortgage-calculator-react";
- import "./Mortgage.css";
+import "./Mortgage.css";
 import { Col, Row, Wrapper } from "../../components/Grid";
+import axios from "axios";
 
 class Mortgage extends Component {
   state = {
-
+    tyf: "",
+    fyf: "",
+    sya: "",
+    fya: "",
+    tyv: ""
   };
   
-  
+
   handleInputChange = event => {
     const { name, value } = event.target;
 
@@ -22,26 +27,57 @@ class Mortgage extends Component {
     event.preventDefault();
 
   };
+  componentDidMount() {
+    axios.get("/api/scrape")
+      .then( (results) => {
+        console.log(results)
+        this.setState({
+          fyf : results.data[0].rates[0],
+          tyf : results.data[0].rates[1],
+          sya : results.data[0].rates[2],
+          fya : results.data[0].rates[3],
+          tyv : results.data[0].rates[4]
+        });
+      })
+
+  }
 
   render() {
     return (
-        <Wrapper>
-            <Row>
-                <Col size="3">
+      <Wrapper>
+        <Row>
+          <Col size="1">
 
-                </Col>
-                <Col size="6">
-                <div className="mc">
-                    {/* <MortgageCalculator styles={customStyle} /> */}
-                    <MortgageCalculator />
+          </Col>
+          <Col size="6">
+            <div className="mc">
+              {/* <MortgageCalculator styles={customStyle} /> */}
+              <MortgageCalculator />
 
-                </div>
-                </Col>
-                <Col size="3">
- 
-                </Col>
-            </Row>
-        </Wrapper>
+            </div>
+          </Col>
+          <Col size="1">
+
+          </Col>
+          <Col size="4">
+            <div className="qlrates">
+              <h4>Today's Mortgage Rates</h4>
+              <p className="ctext">Courtesy of <a className="findHome" href="https://www.quickenloans.com/mortgage-rates?qlsource=nav" target="_blank">QuickenLoans</a></p>
+              <ul>
+                <li>30 Year Fixed: {this.state.tyf}%</li>
+                <li>15 Year Fixed: {this.state.fyf}%</li>
+                <li> 7 Year ARM: {this.state.sya}%</li>
+                <li> 5 Year ARM: {this.state.fya}%</li>
+                <li>30 Year VA: {this.state.tyv}%</li>
+              </ul>
+              <br />
+              <a className="findHome" href="https://www.zillow.com/" target="_blank">Find a Home on Zillow</a>
+              <br />
+              <a className="findHome" href ="https://www.realtor.com/realestateagents" target="_blank">Find a Realtor</a>
+            </div>
+          </Col>
+        </Row>
+      </Wrapper>
     );
   }
 }
